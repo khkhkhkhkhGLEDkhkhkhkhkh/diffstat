5cb851b6ea82800b3e7764efdc1475bb9b171bc4
pplcanfly <sleep.etc@gmail.com>
2011-04-29 15:06:50 +0200

    parse method implementation

--- lib/diffstat.rb
+++ lib/diffstat.rb
@@ -5,9 +5,42 @@ module DiffStat
     end
 
     def parse
-      []
+      result = []
+      stat = nil
+      previous = nil
+      @diff.each_line do |line|
+        case line
+        when /^\+{3} (.*) \(revision .*/
+          stat = Stat.new
+          stat.name = $1
+          previous = nil
+          result << stat
+        when /^\-{3} .*/
+
+        when /^ .*/
+          previous = :c
+        when /^\+.*/
+          stat.additions += 1 if previous != :-
+          if previous == :-
+            stat.modifications += 1
+            stat.deletions -= 1
+          end
+          previous = :+
+        when /^\-.*/
+          stat.deletions += 1
+          previous = :-
+        end
+      end
+      result
     end
   end
 
-  Stat = Struct.new(:name, :additions, :deletions, :modifications)
+  class Stat
+    attr_accessor :name, :additions, :deletions, :modifications
+
+    def initialize
+      @name, @additions, @deletions, @modifications = '', 0, 0, 0
+    end
+  end
+
 end
